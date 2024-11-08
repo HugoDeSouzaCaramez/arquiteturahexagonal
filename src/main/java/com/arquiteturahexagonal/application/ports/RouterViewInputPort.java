816d2e9552f412a41763f4043da93749856ccd91
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-11-08 19:11:15 -0300

    Portas de entrada

---
+++ src/main/java/com/arquiteturahexagonal/application/ports/RouterViewInputPort.java
@@ -0,0 +1,20 @@
+package com.arquiteturahexagonal.application.ports;
+
+import com.arquiteturahexagonal.application.usecase.RouterViewUseCase;
+import com.arquiteturahexagonal.domain.Router;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public class RouterViewInputPort implements RouterViewUseCase {
+    private RouterViewOutputPort routerListOutputPort;
+    public RouterViewInput
+    Port(RouterViewOutputPort routerViewOutputPort) {
+        this.routerListOutputPort = routerViewOutputPort;
+    }
+    @Override
+    public List<Router> getRouters(Predicate<Router> filter) {
+        var routers = routerListOutputPort.fetchRouters();
+        return Router.retrieveRouter(routers, filter);
+    }
+}
--- src/main/java/com/arquiteturahexagonal/application/usecase/RouterViewUseCase.java
+++ src/main/java/com/arquiteturahexagonal/application/usecase/RouterViewUseCase.java
@@ -1,5 +1,10 @@
 package com.arquiteturahexagonal.application.usecase;
 
+import com.arquiteturahexagonal.domain.Router;
+
+import java.util.List;
+import java.util.function.Predicate;
+
 public interface RouterViewUseCase {
     List<Router> getRouters(Predicate<Router> filter);
 }
