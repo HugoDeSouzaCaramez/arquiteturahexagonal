1703968f3c516f8aaa32b5affcf8af475888df3c
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-11-08 19:30:30 -0300

    resolvendo bug de importação

--- src/main/java/com/arquiteturahexagonal/framework/adapters/input/stdin/RouterViewCLIAdapter.java
+++ src/main/java/com/arquiteturahexagonal/framework/adapters/input/stdin/RouterViewCLIAdapter.java
@@ -3,6 +3,9 @@ package com.arquiteturahexagonal.framework.adapters.input.stdin;
 import com.arquiteturahexagonal.application.ports.input.RouterViewInputPort;
 import com.arquiteturahexagonal.application.usecase.RouterViewUseCase;
 import com.arquiteturahexagonal.domain.Router;
+import com.arquiteturahexagonal.framework.adapters.output.file.RouterViewFileAdapter;
+
+import java.util.List;
 
 public class RouterViewCLIAdapter {
     private RouterViewUseCase routerViewUseCase;
