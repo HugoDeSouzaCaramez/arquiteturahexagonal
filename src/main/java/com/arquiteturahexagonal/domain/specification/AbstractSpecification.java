95d294469d678a419ee1ba1500ca1326ebd68283
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-11-09 16:32:57 -0300

    Especificação de domínio que implemente Specification
    É por meio da implementação isSatisfiedBy que definiremos nossos predicados. Seguido por essa
    interface, precisamos criar uma classe abstrata que implemente o método and para nos permitir
    combinar especificações.

---
+++ src/main/java/com/arquiteturahexagonal/domain/specification/AbstractSpecification.java
@@ -0,0 +1,11 @@
+package com.arquiteturahexagonal.domain.specification;
+
+public abstract class AbstractSpecification<T> implements
+        Specification<T> {
+    public abstract boolean isSatisfiedBy(T t);
+    public Specification<T> and(final Specification<T>
+                                        specification) {
+        return new AndSpecification<T>(this,
+                specification);
+    }
+}
