346f0e95338ffd9e3c24c3eaa0f58b018a98fa95
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-11-09 17:15:16 -0300

    Refatoração
    Depois de terminarmos de ajustar a palavra-chave final nas classes de especificação de implementação, teremos
    um conjunto bem definido de classes/interfaces seladas, descrevendo quais classes de especificação podem ser
    usadas para definir as regras de negócios de um sistema.
    Agora estamos prontos para refatorar nosso serviço de domínio, responsável por criar novas redes para usar
    essas especificações

--- src/main/java/com/arquiteturahexagonal/domain/service/NetworkOperation.java
+++ src/main/java/com/arquiteturahexagonal/domain/service/NetworkOperation.java
@@ -1,17 +1,28 @@
 package com.arquiteturahexagonal.domain.service;
 
-//
 public class NetworkOperation {
-    final private int MINIMUM_ALLOWED_CIDR = 8;
     public void createNewNetwork(Router router, IP
             address, String name, int cidr) {
-        if(cidr < MINIMUM_ALLOWED_CIDR)
-            throw new IllegalArgumentException("CIDR is below "+MINIMUM_ALLOWED_CIDR);
-        if(isNetworkAvailable(router, address))
-            throw new IllegalArgumentException("Address already exist");
-                    Network =
-                            router.createNetwork(address,name,cidr);
-        router.addNetworkToSwitch(network);
+        var availabilitySpec = new
+                NetworkAvailabilitySpecification(address, name,
+                cidr);
+        var cidrSpec = new CIDRSpecification();
+        var routerTypeSpec = new
+                RouterTypeSpecification();
+        var amountSpec = new
+                NetworkAmountSpecification();
+        if(cidrSpec.isSatisfiedBy(cidr))
+            throw new IllegalArgumentException("CIDR is
+                    below
+                    "+CIDRSpecification.MINIMUM_ALLOWED_CIDR);
+        if(availabilitySpec.isSatisfiedBy(router))
+            throw new IllegalArgumentException("Address
+                    already exist");
+        if(amountSpec.and(routerTypeSpec).isSatisfiedBy
+                (router)) {
+            Network network =
+                    router.createNetwork(address, name, cidr);
+            router.addNetworkToSwitch(network);
+        }
     }
 }
-
