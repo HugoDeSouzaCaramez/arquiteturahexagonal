867e713fee420a0422233d8e7e3ccb35d398e69b
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-11-09 15:40:19 -0300

    Objeto entidade de domínio

---
+++ src/main/java/com/arquiteturahexagonal/domain/entity/Entity.java
@@ -0,0 +1,48 @@
+package com.arquiteturahexagonal.domain.entity;
+
+import java.time.OffsetDateTime;
+
+public class Event implements Comparable<Event> {
+
+    private final OffsetDateTime timestamp;
+    private final EventId id;
+    private final Protocol protocol;
+    private final Activity activity;
+
+    public Event(OffsetDateTime timestamp, EventId id, Protocol protocol, Activity activity){
+        this.timestamp = timestamp;
+        this.id = id;
+        this.protocol = protocol;
+        this.activity = activity;
+    }
+
+    public static Event parsedEvent(String unparsedEvent, ParsePolicyType policy){
+        return switch (policy) {
+            case REGEX -> new RegexEventParser().parseEvent(unparsedEvent);
+            case SPLIT -> new SplitEventParser().parseEvent(unparsedEvent);
+        };
+    }
+
+    @Override
+    public int compareTo(Event event) {
+        return timestamp.compareTo(event.timestamp);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Event event) {
+            return (event.timestamp.equals(this.timestamp)
+                    && event.id.equals(this.id)
+                    && event.protocol.equals(this.protocol)
+                    && event.activity.equals(this.activity)
+            );
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(timestamp, id, protocol, activity) + 31;
+    }
+}
