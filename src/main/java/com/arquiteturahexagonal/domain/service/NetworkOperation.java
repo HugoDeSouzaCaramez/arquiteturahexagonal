33a11cfe6034010ac3befd5a804928faab28d827
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-11-09 16:30:24 -0300

    Especificação de domínio Specification
    especificações são como condições ou predicados usados para garantir as propriedades de um objeto.
    No entanto, o que caracteriza uma especificação é seu cuidado em encapsular esses predicados de forma
    mais expressiva do que meros operadores lógicos. Uma vez encapsuladas, essas especificações podem ser
    reutilizadas e até combinadas para expressar melhor o domínio do problema.

--- src/main/java/com/arquiteturahexagonal/domain/service/NetworkOperation.java
+++ src/main/java/com/arquiteturahexagonal/domain/service/NetworkOperation.java
@@ -6,11 +6,9 @@ public class NetworkOperation {
     public void createNewNetwork(Router router, IP
             address, String name, int cidr) {
         if(cidr < MINIMUM_ALLOWED_CIDR)
-            throw new IllegalArgumentException("CIDR is
-                    below "+MINIMUM_ALLOWED_CIDR);
+            throw new IllegalArgumentException("CIDR is below "+MINIMUM_ALLOWED_CIDR);
         if(isNetworkAvailable(router, address))
-            throw new IllegalArgumentException("Address already
-                    exist");
+            throw new IllegalArgumentException("Address already exist");
                     Network =
                             router.createNetwork(address,name,cidr);
         router.addNetworkToSwitch(network);
