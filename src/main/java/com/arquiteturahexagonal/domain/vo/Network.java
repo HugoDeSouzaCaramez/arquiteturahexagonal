b76083c0cda84e28e8fdc318fd432d51bd468f68
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-11-09 15:53:15 -0300

    Objeto de valor e agregação

---
+++ src/main/java/com/arquiteturahexagonal/domain/vo/Network.java
@@ -0,0 +1,11 @@
+package com.arquiteturahexagonal.domain.vo;
+
+public record Network(IP address, String name, int cidr) {
+    public Network {
+        if (cidr < 1 || cidr > 32) {
+            throw new IllegalArgumentException("Invalid
+                    CIDR
+                    value");
+        }
+    }
+}
