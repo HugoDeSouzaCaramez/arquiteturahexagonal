efe970f69e1ea6780ade58c706b1bc15025af3b0
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-11-10 17:51:39 -0300

    Update RouterNetworkOutputPort.java

--- src/main/java/com/arquiteturahexagonal/application/ports/output/RouterNetworkOutputPort.java
+++ src/main/java/com/arquiteturahexagonal/application/ports/output/RouterNetworkOutputPort.java
@@ -3,6 +3,11 @@ package com.arquiteturahexagonal.application.ports.output;
 import com.arquiteturahexagonal.domain.entity.Router;
 import com.arquiteturahexagonal.domain.vo.RouterId;
 
+/**
+ * Esta porta de saída informa que o aplicativo pretende obter e manter dados de fontes externas.
+ * O sistema hexagon não sabe se a fonte externa é um banco de dados, um arquivo plano ou outro sistema.
+ * Aqui, apenas declaramos a intenção de obter dados de fora.
+ * */
 public interface RouterNetworkOutputPort {
     Router fetchRouterById(RouterId routerId);
 
