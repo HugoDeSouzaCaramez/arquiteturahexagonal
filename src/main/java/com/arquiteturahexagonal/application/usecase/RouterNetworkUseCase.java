916b2e9ed032ea3dfd1582649e451527b4333f09
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-11-10 17:29:12 -0300

    Update RouterNetworkUseCase.java
    O papel dos casos de uso na arquitetura hexagonal é que eles nos permitem implementar portas de entrada. É por
    meio de portas de entrada que construímos a lógica que irá, por exemplo, chamar serviços hexagonais de domínio,
    outros casos de uso e recursos externos por meio de portas de saída

--- src/main/java/com/arquiteturahexagonal/application/usecase/RouterNetworkUseCase.java
+++ src/main/java/com/arquiteturahexagonal/application/usecase/RouterNetworkUseCase.java
@@ -4,6 +4,7 @@ import com.arquiteturahexagonal.domain.entity.Router;
 import com.arquiteturahexagonal.domain.vo.Network;
 import com.arquiteturahexagonal.domain.vo.RouterId;
 
+//
 public interface RouterNetworkUseCase {
     Router addNetworkToRouter(RouterId routerId, Network network);
 }
