517b307dc5c9e4d7066fdc1fe1196dd44fd41306
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-11-10 17:53:52 -0300

    Update RouterNetworkInputPort.java

--- src/main/java/com/arquiteturahexagonal/application/ports/input/RouterNetworkInputPort.java
+++ src/main/java/com/arquiteturahexagonal/application/ports/input/RouterNetworkInputPort.java
@@ -7,6 +7,17 @@ import com.arquiteturahexagonal.domain.service.NetworkOperation;
 import com.arquiteturahexagonal.domain.vo.Network;
 import com.arquiteturahexagonal.domain.vo.RouterId;
 
+
+/**
+ * O método addNetworkToRouter , que retorna um objeto Router , é o único método público que é exposto pela porta de
+ * entrada. Tornamos todos os outros métodos privados porque eles não devem ser usados fora do contexto desta
+ * porta de entrada. A porta de entrada inicia seu trabalho usando RouterId para recuperar um objeto Router ; então, ela
+ * cria um novo objeto Network naquele objeto Router . Lembre-se, o objeto Network compreende os atributos address,
+ * name e CIDR, conforme expresso na forma escrita do caso de uso. O método fetchRouter tentará obter um objeto
+ * Router passando um ID RouterId para o método fetchRouterById da porta de saída . É quando a porta de entrada
+ * precisará coordenar uma chamada externa que será realizada por um adaptador de saída que implementa a porta de
+ * saída.
+ * */
 public class RouterNetworkInputPort implements RouterNetworkUseCase {
     private final RouterNetworkOutputPort
             routerNetworkOutputPort;
