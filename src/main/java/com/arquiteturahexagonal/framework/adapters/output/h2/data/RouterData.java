5f146838a9a954911423ac90379bb785bc51127a
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-11-11 19:48:43 -0300

    Adaptador de saída
    Usamos as anotações @Getter, @NoArgsConstructor e @AllArgsConstructor do Lombok para diminuir a verbosidade
    da classe. Usaremos os getters e o construtor mais tarde para converter a classe de dados em uma classe de
    modelo de domínio.
    O uso das anotações @Table e @SecondaryTable serve para representar o relacionamento entre as tabelas de
    roteadores e switches . Esse relacionamento é mapeado usando as anotações @OntToOne e @JoinColumn ,
    especificando que ambas as tabelas devem ser vinculadas por meio do atributo router_id .

---
+++ src/main/java/com/arquiteturahexagonal/framework/adapters/output/h2/data/RouterData.java
@@ -0,0 +1,42 @@
+package com.arquiteturahexagonal.framework.adapters.output.h2.data;
+
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.eclipse.persistence.annotations.Convert;
+import org.eclipse.persistence.annotations.Converter;
+import java.io.Serializable;
+import java.util.UUID;
+
+/**
+ * Lembre-se, nosso modelo de domínio vem primeiro, então não queremos acoplar o sistema com tecnologia de
+ * banco de dados. É por isso que precisamos criar uma classe RouterData ORM para mapear diretamente para os
+ * tipos de banco de dados.
+ * */
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "routers")
+@SecondaryTable(name = "switches")
+@MappedSuperclass
+@Converter(name="uuidConverter", converterClass = UUIDTypeConverter.class)
+public class RouterData implements Serializable {
+    @Column(name="router_id", columnDefinition = "uuid", updatable = false )
+    @Convert("uuidConverter")
+    private UUID routerId;
+    @Embedded
+    @Enumerated(EnumType.STRING)
+    @Column(name="router_type")
+    private RouterTypeData routerType;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(table = "switches",
+            name = "router_id",
+            referencedColumnName = "router_id")
+    private SwitchData networkSwitch;
+}
