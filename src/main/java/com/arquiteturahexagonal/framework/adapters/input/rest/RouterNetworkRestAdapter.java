5c59d46372a194b6ef9b6d21655dffbf5ed28641
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-11-11 17:25:09 -0300

    método processReques
    Este método recebe um objeto httpServer , que é usado para criar o ponto de extremidade HTTP
    para receber solicitações GET em /network/add.
    O adaptador REST recebe dados do usuário por meio de uma solicitação HTTP, analisa os parâmetros da solicitação e os
    usa para chamar addNetworkToRouter definido na classe pai RouterNetworkAdapter.
    embre-se de que o adaptador de entrada é responsável por converter dados do usuário em parâmetros adequados
    usados para acionar uma porta de entrada usando sua referência de caso de uso.
    Neste momento, os dados saem do hexágono Framework e vão para o hexágono Application.

--- src/main/java/com/arquiteturahexagonal/framework/adapters/input/rest/RouterNetworkRestAdapter.java
+++ src/main/java/com/arquiteturahexagonal/framework/adapters/input/rest/RouterNetworkRestAdapter.java
@@ -1,12 +1,50 @@
 package com.arquiteturahexagonal.framework.adapters.input.rest;
 
 import com.arquiteturahexagonal.application.usecase.RouterNetworkUseCase;
+import com.arquiteturahexagonal.domain.entity.Router;
 import com.arquiteturahexagonal.framework.adapters.input.RouterNetworkAdapter;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RouterNetworkRestAdapter extends RouterNetworkAdapter {
 
     public RouterNetworkRestAdapter(RouterNetworkUseCase routerNetworkUseCase) {
         this.routerNetworkUseCase = routerNetworkUseCase;
+    }
+
+    /**
+     * When implementing a REST adapter, the processRequest method receives an Object type parameter
+     * that is always cast to an HttpServer type.
+     */
+    @Override
+    public Router processRequest(Object requestParams){
+        Map<String, String> params = new HashMap<>();
+        if(requestParams instanceof HttpServer) {
+            var httpserver = (HttpServer) requestParams;
+            httpserver.createContext("/network/add", (exchange -> {
+                if ("GET".equals(exchange.getRequestMethod())) {
+                    var query = exchange.getRequestURI().getRawQuery();
+                    httpParams(query, params);
+                    router = this.addNetworkToRouter(params);
+                    ObjectMapper mapper = new ObjectMapper();
+                    var routerJson = mapper.writeValueAsString(RouterJsonFileMapper.toJson(router));
+                    exchange.getResponseHeaders().set("Content-Type", "application/json");
+                    exchange.sendResponseHeaders(200, routerJson.getBytes().length);
+                    OutputStream output = exchange.getResponseBody();
+                    output.write(routerJson.getBytes());
+                    output.flush();
+                } else {
+                    exchange.sendResponseHeaders(405, -1);
+                }
+                exchange.close();
+            }));
+            httpserver.setExecutor(null);
+            httpserver.start();
+        }
+        return router;
     }
 }
 
