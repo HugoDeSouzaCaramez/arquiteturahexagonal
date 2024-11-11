9ded0dfb4a5a3923c1b33e7770bf1b6d4f454a07
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-11-11 17:52:55 -0300

    Chamando os adaptadores de entrada
    Aqui está o código do cliente para controlar qual adaptador escolher.
    Se passarmos rest como parâmetro ao executar o programa, a condição switch-case criará
    uma instância do adaptador REST e chamará o método rest.
    O método rest , por sua vez, chama o método processRequest do adaptador de entrada REST.
    aplicativo executando o seguinte comando:
    2. Para chamar o adaptador REST, execute o arquivo .jar com o parâmetro rest :
    Caso contrário, se passarmos o parâmetro cli ao executar o programa, o switch-case criará, por
    padrão, um adaptador CLI e chamará o método cli.
    O método cli então chama o método processRequest do adaptador de entrada CLI.

--- src/main/java/com/arquiteturahexagonal/App.java
+++ src/main/java/com/arquiteturahexagonal/App.java
@@ -1,16 +1,65 @@
 package com.arquiteturahexagonal;
 
-import com.arquiteturahexagonal.framework.adapters.input.stdin.RouterViewCLIAdapter;
+import com.arquiteturahexagonal.application.ports.input.RouterNetworkInputPort;
+import com.arquiteturahexagonal.application.ports.output.RouterNetworkOutputPort;
+import com.arquiteturahexagonal.application.usecase.RouterNetworkUseCase;
+import com.arquiteturahexagonal.framework.adapters.input.RouterNetworkAdapter;
+import com.arquiteturahexagonal.framework.adapters.input.rest.RouterNetworkRestAdapter;
+import com.arquiteturahexagonal.framework.adapters.input.stdin.RouterNetworkCLIAdapter;
+import com.arquiteturahexagonal.framework.adapters.output.file.RouterNetworkFileAdapter;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.Scanner;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main(String... args) {
-        var cli = new RouterViewCLIAdapter();
-        var type = "CORE";
-        System.out.println(cli.obtainRelatedRouters(type));
+public class App {
+
+    private RouterNetworkAdapter inputAdapter;
+    private RouterNetworkUseCase usecase;
+    private RouterNetworkOutputPort outputPort;
+
+    public static void main(String... args)  {
+        var adapter = "cli";
+        if(args.length>0) {
+            adapter = args[0];
+        }
+        new App().setAdapter(adapter);
+    }
+
+    void setAdapter(String adapter) {
+        switch (adapter) {
+            case "rest" -> {
+                outputPort = RouterNetworkH2Adapter.getInstance();
+                usecase = new RouterNetworkInputPort(outputPort);
+                inputAdapter = new RouterNetworkRestAdapter(usecase);
+                rest();
+            }
+            default -> {
+                outputPort = RouterNetworkFileAdapter.getInstance();
+                usecase = new RouterNetworkInputPort(outputPort);
+                inputAdapter = new RouterNetworkCLIAdapter(usecase);
+                cli();
+            }
+        }
+    }
+
+    private void cli() {
+        Scanner scanner = new Scanner(System.in);
+        inputAdapter.processRequest(scanner);
+    }
+
+    private void rest() {
+        try {
+            System.out.println("REST endpoint listening on port 8080...");
+            var httpserver = HttpServer.create(new InetSocketAddress(8080), 0);
+            inputAdapter.processRequest(httpserver);
+        } catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
