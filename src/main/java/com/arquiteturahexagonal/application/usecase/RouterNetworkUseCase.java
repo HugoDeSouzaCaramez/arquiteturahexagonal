8b31cc74e52b6d50ef4728877a3589eff14d165f
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-11-13 16:58:27 -0300

    Adicionando o método getRouter a RouterNetworkUseCase
    Vamos começar a aprimorar o aplicativo hexagonal adicionando o método getRouter a interface RouterNetworkUseCase

--- src/main/java/com/arquiteturahexagonal/application/usecase/RouterNetworkUseCase.java
+++ src/main/java/com/arquiteturahexagonal/application/usecase/RouterNetworkUseCase.java
@@ -4,8 +4,11 @@ import com.arquiteturahexagonal.domain.entity.Router;
 import com.arquiteturahexagonal.domain.vo.Network;
 import com.arquiteturahexagonal.domain.vo.RouterId;
 
-//
 public interface RouterNetworkUseCase {
+
     Router addNetworkToRouter(RouterId routerId, Network network);
+
+    Router getRouter(RouterId routerId);
 }
+
 
