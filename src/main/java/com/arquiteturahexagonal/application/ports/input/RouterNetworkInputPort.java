f0b4c5a330cefb788111a94980edf85dbde4c637
HugoDeSouzaCaramez <hugo.7.musica@gmail.com>
2024-11-14 12:27:28 -0300

    Update RouterNetworkInputPort.java
    A última coisa que precisamos fazer no aplicativo hexagonal é fazer com que os métodos
    addNetworkToRouter e getRouter enviem eventos usando as portas e adaptadores que acabamos de criar.
    Observe que agora estamos chamando sendEvent em ambos os métodos (addNetworkToRouter e
    getRouter), então sempre que adicionarmos uma rede ou recuperarmos um roteador, o aplicativo
    hexagonal enviará um evento nos informando o que aconteceu.
    Agora podemos adicionar uma página Events para permitir que o aplicativo frontend se conecte ao
    servidor WebSocket do aplicativo hexagonal.

--- src/main/java/com/arquiteturahexagonal/application/ports/input/RouterNetworkInputPort.java
+++ src/main/java/com/arquiteturahexagonal/application/ports/input/RouterNetworkInputPort.java
@@ -64,31 +64,49 @@ import com.arquiteturahexagonal.domain.vo.RouterId;
  * addNetworkToRouter , mas agora também por getRouter.
  * */
 public class RouterNetworkInputPort implements RouterNetworkUseCase {
-    private final RouterNetworkOutputPort
-            routerNetworkOutputPort;
-    public RouterNetworkInputPort(RouterNetworkOutputPort
-                                          routerNetworkOutputPort){
-        this.routerNetworkOutputPort =
-                routerNetworkOutputPort;
+
+    private RouterNetworkOutputPort routerNetworkOutputPort;
+
+    private NotifyEventOutputPort notifyEventOutputPort;
+
+    public RouterNetworkInputPort(RouterNetworkOutputPort routerNetworkOutputPort, NotifyEventOutputPort notifyEventOutputPort){
+        this.routerNetworkOutputPort = routerNetworkOutputPort;
+        this.notifyEventOutputPort = notifyEventOutputPort;
     }
+
+    public RouterNetworkInputPort(RouterNetworkOutputPort routerNetworkOutputPort){
+        this.routerNetworkOutputPort = routerNetworkOutputPort;
+    }
+
     @Override
     public Router addNetworkToRouter(RouterId routerId, Network network) {
         var router = fetchRouter(routerId);
+        notifyEventOutputPort.sendEvent("Adding "+network.getName()+" network to router "+router.getRouterId().getUUID());
         return createNetwork(router, network);
     }
+
+    @Override
     public Router getRouter(RouterId routerId) {
+        notifyEventOutputPort.sendEvent("Retrieving router ID "+routerId.getUUID());
         return fetchRouter(routerId);
     }
+
     private Router fetchRouter(RouterId routerId) {
         return routerNetworkOutputPort.fetchRouterById(routerId);
     }
 
     private Router createNetwork(Router router, Network network) {
-        var newRouter = NetworkOperation.createNewNetwork(router, network);
-        return persistNetwork(router) ? newRouter : router;
+        try {
+            var routerWithNewNetwork = NetworkOperation.createNewNetwork(router, network);
+            return persistNetwork(routerWithNewNetwork) ?
+                    routerWithNewNetwork: router;
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            throw e;
+        }
     }
+
     private boolean persistNetwork(Router router) {
         return routerNetworkOutputPort.persistRouter(router);
     }
 }
-
